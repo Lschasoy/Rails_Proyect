774686d6ad0ef5022d6c57d37fb7a93cd00b5285
Sergio Jesus Garcia Llarena <sergiojgl@gmail.com>
2012-04-10 11:11:53 +0100

    Test pasados

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,22 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class User < ActiveRecord::Base
+    attr_accessible :name, :email
+
+    validates :name, :presence => true,
+                     :length => { :maximum => 25 }
+
+    email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+    validates :email, :presence => true,
+                     :format => { :with => email_regex }
 end

9b99be35a47e43665b14854f43abf357028282dc
RC10 <thelonelywolf88@gmail.com>
2012-04-11 13:06:33 +0100

    Mas pruebas superadas (no todas)

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@
 #  updated_at :datetime
 #
 
+require 'digest'
 class User < ActiveRecord::Base
     attr_accessor :password
     attr_accessible :name, :email, :password, :password_confirmation
@@ -25,4 +26,38 @@ class User < ActiveRecord::Base
     validates :password, :presence => true,
                          :confirmation => true,
                          :length => { :within => 6..40 }
+
+    before_save :encrypt_password
+ 
+    #Hay contraseña?
+    def has_password?(submitted_password)
+        #Se comparara la contraseña encriptada con la enviada
+        encrypted_password == encrypt(submitted_password)
+    end
+ 
+    #Autentificacion
+    def self.authenticate(email, submitted_password)
+       user = find_by_email(email)
+       return nil if user.nil?
+       return user if user.has_password?(submitted_password)
+    end
+
+    private
+  
+       def encrypt_password
+          self.salt = make_salt unless has_password?(password)
+          self.encrypted_password = encrypt(password)
+       end
+
+       def encrypt(string)
+          secure_hash("#{salt}--#{string}")
+       end
+
+       def make_salt
+          secure_hash("#{Time.now.utc}--#{password}")
+       end
+
+       def secure_hash(string)
+          Digest::SHA2.hexdigest(string)
+       end
 end

eda353765c846fcc04dcf65e258e3caf63464e0e
Sergio Jesus Garcia Llarena <sergiojgl@gmail.com>
2012-05-22 17:38:23 +0100

    Terminado css (de momento)

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,19 @@
 module UsersHelper
 
+#   def gravatar_for(user, options = { :size => 50 })
+#     gravatar_image_tag(user.email.downcase, :alt => h(user.name),
+#                                             :class => 'gravatar',
+#                                             :gravatar => options)
+#   end
+  
   def gravatar_for(user, options = { :size => 50 })
-    gravatar_image_tag(user.email.downcase, :alt => h(user.name),
-                                            :class => 'gravatar',
-                                            :gravatar => options)
+    h = { 0 => "identicon", 1 => "monsterid", 2 => "wavatar", 3 => "retro" }
+    options["d"]=h[3-user.id%4]
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "http://gravatar.com/avatar/#{gravatar_id}"
+    gravatar_image_tag(gravatar_url, :alt => h(user.name),
+                                     :class => 'gravatar',
+                                     :gravatar => options)
   end
+  
 end

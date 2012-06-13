02a6dd88919d4689fdcb8acb3db2afd6f546af9d
Sergio Jesus Garcia Llarena <sergiojgl@gmail.com>
2012-06-13 15:16:28 +0100

    Proyecto terminado

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,9 +3,10 @@ module UsersHelper
   def gravatar_for(user, options = { :size => 50 })
     h = { 0 => "identicon", 1 => "monsterid", 2 => "wavatar", 3 => "retro" }
     options["d"]=h[3-user.id%4]
-    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    email = user.email
+    gravatar_id = Digest::MD5.hexdigest("sergiojgl@gmail.com")
     gravatar_url = "http://gravatar.com/avatar/#{gravatar_id}"
-    gravatar_image_tag(gravatar_url, :alt => h(user.name),
+    gravatar_image_tag(gravatar_url, :alt => email,
                                      :class => 'gravatar',
                                      :gravatar => options)
   end
